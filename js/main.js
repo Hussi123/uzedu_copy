f4d41288893708a2855699bd02fe5bb8eebb9a57
Abdullayev Husniddin <husniddinabdullayev936@gmail.com>
2024-11-26 21:07:06 +0500

    this is second day additional functionals finished and languages selector finished

--- js/main.js
+++ js/main.js
@@ -1,23 +1,42 @@
-const langBtn =  document.querySelector('.dropdown-btn')
+// search iconkasiga click bo'lganda search inputni ochish va close iconkasiga click bo'lganda input oynasini tozalash
+const searchBtnId = document.querySelector("#searchBtnId")
+const formId = document.querySelector("#formId")
+const idFormCloseBtn = document.querySelector("#idFormCloseBtn")
+searchBtnId.addEventListener("click", () => {
+  formId.classList.add("active")
+});
+idFormCloseBtn.addEventListener("click", () => {
+  formId.classList.remove("active")
+})
 
+
+// languades choose Uzb/Ўзб/Ru div
+const langBtn = document.querySelector('.dropdown-btn')
+const content = document.querySelector('.dropdown-content');
+
+// languages dropdown hide and show funcktion
 langBtn.addEventListener('click', function () {
-  const content = document.querySelector('.dropdown-content');
   content.style.display = content.style.display === 'block' ? 'none' : 'block';
-
-  // .forEach(element => {
-  //   if(element.textContent == "O‘zbekcha") {
-  //     langBtn.textContent=="Uzb"
-  //   }
-  //   else if (element.textContent == "Ўзбекча"){
-  //     langBtn.textContent=="Ўзб"
-  //   }
-  //   else if (element.textContent == "Русский"){
-  //     langBtn.textContent=="Pu"
-  //   }
-  // });
-  
+  // console.log(content);
 });
 
-const langBarBtn = document.querySelector("button")
+content.addEventListener("click", (e) => {
 
-console.log(langBarBtn.style);
+  content.querySelectorAll("a").forEach(e => e.classList.remove("active"))
+
+  if (e.target.tagName === "A") {
+    if (e.target.innerText == "O‘zbekcha"){
+      langBtn.innerText="Uzb"
+      e.target.classList.add("active")
+    }else if (e.target.innerText == "Ўзбекча"){
+      langBtn.innerText="Ўзб"
+      e.target.classList.add("active")
+    }else if(e.target.innerText == "Русский"){
+      langBtn.innerText="Pu"
+      e.target.classList.add("active")
+    }
+  }
+})
+
+
+
